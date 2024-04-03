9a2ba7e5ce3057d26f2c55cd5f27bec5249ed5a9
l.eschweiler <l.eschweiler@novacapta.de>
2024-04-03 18:37:08 +0200

    discord anpassungen nach absprache wp treff köln

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wp-plugin-erfindergeist
 Wordpress Plugin Erfindergeist
+
+## includes plugin
+- erfindergeist-discord
+- erfindergeist-g-calendar
--- erfindergeist-discord/styles.php
+++ erfindergeist-discord/styles.php
@@ -1,24 +1,22 @@
 <?php
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+  exit;
 }
 
 // we need this to load the parent styles
 // https://developer.wordpress.org/themes/advanced-topics/child-themes/
-function erfindergeist_discord_styles()
+function erfindergeistDiscordStyles()
 {
    wp_enqueue_style(
       'discord-style',
-      // get_stylesheet_directory_uri() . '/styles/discord.css',
       plugins_url( '/', __FILE__ ) . 'discord.css',
       array(),
       1.0
    );
 
-   wp_enqueue_script( 
+   wp_enqueue_script(
       'discord-script',
-      // get_stylesheet_directory_uri() . '/js/discord.js',
       plugins_url( '/',  __FILE__ ) . 'discord.js',
       array('jquery'),
       1.3,
@@ -26,4 +24,4 @@ function erfindergeist_discord_styles()
    );
 }
 
-add_action('wp_enqueue_scripts', 'erfindergeist_discord_styles');
+add_action('wp_enqueue_scripts', 'erfindergeistDiscordStyles');
