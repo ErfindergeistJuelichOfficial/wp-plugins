95195f8cdf3480440625384d6f2fa6a0c757106e
Lars Eschweiler <vreezy117@googlemail.com>
2025-02-13 20:02:18 +0100

    started

--- erfindergeist-g-calendar/apis.php
+++ erfindergeist-g-calendar/apis.php
@@ -18,26 +18,52 @@ function gcalendar($request)
 
   $apikey_opt_name = 'g_Calendar_apikey';
   $google_calendar_id_opt_name = 'g_Calendar_id';
-
-  if(!get_option( $apikey_opt_name ) && !get_option(  $google_calendar_id_opt_name )) {
-    return new WP_Error('rest_custom_error', 'Apikey is not set', array('status' => 400));
-  }
+  $cache_container_name = 'g_Calendar_cache_container';
+  $cache_timestamp_name = 'g_Calendar_timestamp';
 
   $dateTime = new DateTime();
-   
-  $currentDate = $dateTime->format(DateTimeInterface::RFC3339);
-   
-  // google api dislike +00:00. replace with Z
-  $currentDate = str_replace("+00:00", "Z", $currentDate);
-  
-  $gCalendarApiKey = get_option( $apikey_opt_name );
-  $gCalendarId = get_option( $google_calendar_id_opt_name );
-  $url = 'https://www.googleapis.com/calendar/v3/calendars/'.$gCalendarId.'/events?maxResults=5&orderBy=startTime&singleEvents=true&timeMin=' . $currentDate . '&key='.$gCalendarApiKey;
-   
-  $content = file_get_contents($url);
- 
-  $response = new WP_REST_Response(json_decode($content, true));
-  $response->set_status(200);
 
+  
+  if(isset($cache_date_name) && isset($cache_container_name))
+  {
+    $old_timestamp = get_option($cache_timestamp_name);
+    $current_timestamp = date_timestamp_get($dateTime);
+
+    if($current_timestamp - $old_timestamp > 1000)
+    {
+      
+    }
+    
+    
+
+  }
+  else
+  {
+    if(!get_option( $apikey_opt_name ) && !get_option(  $google_calendar_id_opt_name )) {
+      return new WP_Error('rest_custom_error', 'Apikey is not set', array('status' => 400));
+    }
+  
+  
+     
+    $currentDate = $dateTime->format(DateTimeInterface::RFC3339);
+     
+    // google api dislike +00:00. replace with Z
+    $currentDate = str_replace("+00:00", "Z", $currentDate);
+    
+    $gCalendarApiKey = get_option( $apikey_opt_name );
+    $gCalendarId = get_option( $google_calendar_id_opt_name );
+    $url = 'https://www.googleapis.com/calendar/v3/calendars/'.$gCalendarId.'/events?maxResults=10&orderBy=startTime&singleEvents=true&timeMin=' . $currentDate . '&key='.$gCalendarApiKey;
+     
+    $content = file_get_contents($url);
+   
+    update_option($cache_container_name, json_encode($content));
+    update_option($cache_date_name, date_timestamp_get($dateTime));
+
+   
+   
+  }
+
+  $response = new WP_REST_Response(json_decode($content, true)); 
+  $response->set_status(200);
   return $response;
 }
