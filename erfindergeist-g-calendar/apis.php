2ec3fb2326bda77e5a6a361429c6191ce51cf8b4
Lars Eschweiler <vreezy117@googlemail.com>
2025-06-22 13:55:47 +0200

    add: nextEvent

--- erfindergeist-g-calendar/apis.php
+++ erfindergeist-g-calendar/apis.php
@@ -4,14 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
   exit;
 }
 
-// CUSTOM APIS
-// https://egj.vreezy.de/wp-json/erfindergeist/v1/gcalendar
-add_action('rest_api_init', function () {
-  register_rest_route('erfindergeist/v1', '/gcalendar', array(
-    'methods'  => 'GET',
-    'callback' => 'gcalendar'
-  ));
-});
+
 
 function gcalendar($request)
 {
@@ -36,8 +29,70 @@ function gcalendar($request)
    
   $content = file_get_contents($url);
  
+  return $content;
+  // $response = new WP_REST_Response(json_decode($content, true));
+  // $response->set_status(200);
+
+  // return $response;
+}
+
+function getCalendar($request)
+{
+  $content = gcalendar($request);
   $response = new WP_REST_Response(json_decode($content, true));
   $response->set_status(200);
 
   return $response;
 }
+
+function getNextEvent($request)
+{
+  $content = gcalendar($request);
+  $obj = json_decode($content, true);
+
+  if(is_array($obj["items"]) && $obj["items"][0]["start"]["dateTime"])
+  {
+
+    // 2025-10-22T18:00:00+02:00
+    $date_time_pieces = explode("T", $obj["items"][0]["start"]["dateTime"]);
+
+    // 2025-10-22
+    $date = $date_time_pieces[0];
+
+    // 2001-03-10
+    $tomorrow = new DateTime();
+    $tomorrow->modify("+1 day");
+    $tomorrow_date = $tomorrow->format("Y-m-d");
+
+    if($date === $tomorrow_date)
+    {
+
+      $newObj = $obj["items"][0];
+      $newObj["starttime"] = (new DateTime($newObj["start"]["dateTime"]))->format("d.m.Y H:i");
+      $response = new WP_REST_Response($newObj);
+      $response->set_status(200);
+      return $response;
+    }
+  }
+
+  $response = new WP_REST_Response();
+  $response->set_status(304);
+  return $response;
+
+}
+
+// CUSTOM APIS
+// https://egj.vreezy.de/wp-json/erfindergeist/v1/gcalendar
+add_action('rest_api_init', function () {
+  register_rest_route('erfindergeist/v1', '/gcalendar', array(
+    'methods'  => 'GET',
+    'callback' => 'getCalendar'
+  ));
+});
+
+add_action('rest_api_init', function () {
+  register_rest_route('erfindergeist/v1', '/nextevent', array(
+    'methods'  => 'GET',
+    'callback' => 'getNextEvent'
+  ));
+});
